939146096db731abf0f60a113205a58b932fa6d0
Robert J. Lang <robert@langorigami.com>
2024-03-23 06:24:38 -0700

    Clean up vertical tab summary messages.

--- js/uc_discounts.js
+++ js/uc_discounts.js
@@ -10,22 +10,22 @@ Backdrop.behaviors.ucCartDiscountPaneSummaries = {
     $('fieldset#edit-pane-uc-discounts', context).backdropSetSummary(function(context) {
       var ret = [];
       if ($('#edit-uc-discounts-checkout-allow-user-codes').is(':checked')) {
-        ret.push(Backdrop.t('Discount codes enabled.'));
+        ret.push(Backdrop.t('Discount codes enabled'));
       }
       else {
-        ret.push(Backdrop.t('No discount codes.'));
+        ret.push(Backdrop.t('No discount codes'));
       }
       if ($('#edit-uc-discounts-checkout-show-messages').is(':checked')) {
-        ret.push(Backdrop.t('Show messages.'));
+        ret.push(Backdrop.t('Show discount messages'));
       }
       else {
-        ret.push(Backdrop.t('No messages.'));
+        ret.push(Backdrop.t('No discout messages'));
       }
       if ($('#edit-uc-discounts-checkout-show-in-cart-contents').is(':checked')) {
-        ret.push(Backdrop.t('Show discounts in cart contents.'));
+        ret.push(Backdrop.t('Show discounts in cart contents'));
       }
       else {
-        ret.push(Backdrop.t('No discounts in cart contents.'));
+        ret.push(Backdrop.t('No discounts in cart contents'));
       }
       return ret.join('<br>');
     });
